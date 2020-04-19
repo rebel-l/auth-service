9b3da40f7324e7f9cf2626e4c5e47861ff0e489d
Lars Gaubisch <l.gaubisch@googlemail.com>
2020-04-19 23:50:21 +0200

    Generated by go-project - Step 8: added go base file for service

---
+++ endpoint/doc/doc.go
@@ -0,0 +1,26 @@
+/*
+This service provides authentication and authorization via OAuth2.
+
+Copyright (C) 2020 Lars Gaubisch
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+package doc
+
+// Init initialises the doc endpoints
+func Init(svc *smis.Service) error {
+  _, err := svc.RegisterFileServer("/doc", http.MethodGet, "endpoint/doc/web")
+  return err
+}
